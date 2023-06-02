410400d0d3d7c40f6b591c22380e914547de4805
Ivan Markov <101876348+ivan-markov-666@users.noreply.github.com>
2023-06-02 14:40:41 +0300

    Update current-user.decorator.ts

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -5,6 +5,6 @@ export const CurrentUser = createParamDecorator(
   (data: never, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
     // Return the currentUser property of the request object.
-    return request.CurrentUser;
+    return request.currentUser;
   },
 );
