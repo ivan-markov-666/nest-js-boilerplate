5d7d7f0e4950255204f464944698812e16b1e105
ivan-markov-666 <ivan.markov@mail.com>
2023-06-09 16:05:24 +0300

    Getting TypeScript to help with mocks. By that way we will be able to mock the 'AuthService' class and test it with fake data. The script for running the unit tests was updated in the 'package.json' file.

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,41 +1,28 @@
-// Import the 'Test' object from the '@nestjs/testing' module. This object is used to create a testing module.
 import { Test } from '@nestjs/testing';
-// Import the 'AuthService' from the 'auth.service.ts' file. This is the service we want to test.
 import { AuthService } from './auth.service';
-// Import the 'UsersService' from the 'users.service.ts' file. This is the service that the 'AuthService' depends on.
 import { UsersService } from './users.service';
+// Import 'Users' entity to use it in the fakeUsersService object below to mock the UsersService dependency in the AuthService class.
+import { Users } from './users.entity';
 
-// Added 'it' block to test the 'AuthService' service.
 it('can create an instance of auth service', async () => {
-  // Create a fake copy of the users service that we can control and inspect later.
-  const fakeUsersService = {
-    // Create a 'find' method that returns an empty array.
+  // Add 'Partial' to the fakeUsersService object to mock the UsersService dependency in the AuthService class.
+  const fakeUsersService: Partial<UsersService> = {
     find: () => Promise.resolve([]),
-    // Create a 'create' method that returns an object with an 'email', and 'password' property.
     create: (email: string, password: string) =>
-      // Return an object with an 'id', 'email', and 'password' property.
-      Promise.resolve({ id: 1, email, password }),
-    // 'unknown' is a type that represents any JavaScript value. We use it here because we don't know what the 'UsersService' is returning.
+      // Add 'as Users' to the end of the object to mock the UsersService dependency in the AuthService class.
+      Promise.resolve({ id: 1, email, password } as Users),
   } as unknown as UsersService;
 
-  // Create a testing module that contains the 'AuthService' and the 'fakeUsersService'.
   const module = await Test.createTestingModule({
-    // Define the providers that the testing module should use.
     providers: [
-      // Add the 'AuthService' to the 'providers' array.
       AuthService,
       {
-        // Add the 'UsersService' to the 'providers' array.
         provide: UsersService,
-        // Use the 'fakeUsersService' as the value for the 'UsersService'.
         useValue: fakeUsersService,
       },
     ],
-    // Compile the testing module.
   }).compile();
 
-  // Get the 'AuthService' from the testing module.
   const service = module.get(AuthService);
-  // Expect the 'service' to be defined.
   expect(service).toBeDefined();
 });
