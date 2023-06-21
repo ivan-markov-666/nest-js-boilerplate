4ea59c8f0014014379290ee4d86c51c5fc6906c7
ivan-markov-666 <ivan.markov@mail.com>
2023-06-21 12:09:32 +0300

    'src\setup-app.ts' file was deleted. 'src\main.ts' file was returned to the original state (before the refactoring). Setup a global validation pipe in the NestJS way. Setup a global cookies session middleware in the NestJS way.

--- learn.md
+++ learn.md
@@ -2,14 +2,14 @@
 That file shows what was done at that branch. Remember, the branches are named with a number prefix. The number prefix indicates the lecture's progress. Starting from the first branch, you will be able to learn how to create a new NestJS project from scratch.  
 
 ## What was done in that lecture:  
-Added new 'test\auth.e2e-spec.ts' file.
-Added new 'src\setup-app.ts' file.
-Refactored 'src\main.ts' file.
+'src\setup-app.ts' file was deleted.
+'src\main.ts' file was returned to the original state (before the refactoring).
+Setup a global validation pipe in the NestJS way.
+Setup a global cookies session middleware in the NestJS way. 
 
 ### Added to the project:  
-1. Added new example of integration test in the 'test\auth.e2e-spec.ts'.  
-2. 'app.use' and 'app.useGlobalPipes' moved from 'src\main.ts' to newly added 'src\setup-app.ts' file. The idea is to make the 'main.ts' file as simple as possible. And we want to use 'app.use' and 'app.useGlobalPipes' in the 'test\auth.e2e-spec.ts' file (in the integration tests). So 'src\main.ts' file was refactored.  
-3. Start the integration tests with the command:  
+1. The integration test example in the 'test\auth.e2e-spec.ts' was refactored to use the NestJS way to run the integration tests.
+2. Setup a global pipe in the 'src\app.module.ts' file. Moving the 'ValidationPipe' method from the 'src\main.ts' file to the 'src\app.module.ts' file.
+3. Setup a global cookie-session middleware in the 'src\app.module.ts' file. Moving the 'cookieSession' method from the 'src\main.ts' file to the 'src\app.module.ts' file.
+4. Start the integration tests with the command:  
 > npm run test:e2e  
-
-!!! ALert. This is not the NestJS way to run the integration tests. We will learn how to run the integration tests in the NestJS way in the next lecture.
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
-import { Module } from '@nestjs/common';
+// Imported the "ValidationPipe" and "MiddlewareConsumer" classes from the '@nestjs/common' package.
+import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
+// Imported the 'APP_PIPE' constant from the '@nestjs/core' package.
+import { APP_PIPE } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,6 +9,9 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { Users } from './users/users.entity';
 import { Report } from './reports/report.entity';
+// Added the "cookie-session" package.
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
@@ -19,6 +25,34 @@ import { Report } from './reports/report.entity';
     ReportsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // Added the "ValidationPipe" class to the "providers" array. This makes the "ValidationPipe" class available to the entire application (globally).
+    {
+      // The "APP_PIPE" constant is a special token that tells Nest to use the "ValidationPipe" class.
+      provide: APP_PIPE,
+      // The "useValue" property is used to pass a value to the "ValidationPipe" class.
+      useValue: new ValidationPipe({
+        // The "whitelist" property is used to remove any properties that are not defined in the DTO.
+        whitelist: true,
+      }),
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  // Added the "configure" method to the "AppModule" class.
+  configure(consumer: MiddlewareConsumer) {
+    // The "consumer" parameter is an instance of the "MiddlewareConsumer" class.
+    consumer
+      // The "apply" method is used to apply middleware to the entire application (globally).
+      .apply(
+        // The "cookieSession" function is used to create a middleware that will be applied to the entire application (globally).
+        cookieSession({
+          // The "keys" property is used to encrypt the cookie.
+          keys: ['asdf'],
+        }),
+      )
+      // The "forRoutes" method is used to specify the routes that the middleware will be applied to.
+      .forRoutes('*');
+  }
+}
