6744f2e1f6d52d99fa58fee7991ee18a4e99ee53
ivan-markov-666 <ivan.markov@mail.com>
2023-06-21 17:01:21 +0300

    Add a test for signing up with a new user and then getting the currently logged in user.

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,46 +7,27 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { Users } from './users/users.entity';
 import { Report } from './reports/report.entity';
-// Importing the 'ConfigModule' and 'ConfigService' from the '@nestjs/config' package to use the environment variables in the application code.
 import { ConfigModule, ConfigService } from '@nestjs/config';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
-    // Added 'ConfigModule.forRoot()' to the '@Module' decorator's 'imports' array to load the environment variables.
     ConfigModule.forRoot({
-      // Added 'isGlobal: true' to make the configuration module available everywhere in the application.
       isGlobal: true,
-      // Added 'envFilePath: `.env.${process.env.NODE_ENV}`' to load the environment variables from the '.env.development', '.env.test', (etc.) files when the application is running in the development, test... (etc.) environments.
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    // Added 'TypeOrmModule.forRootAsync()' to the '@Module' decorator's 'imports' array to load the environment variables from the 'ConfigService'.
     TypeOrmModule.forRootAsync({
-      // Injecting the 'ConfigService' into the 'TypeOrmModule.forRootAsync()' method.
       inject: [ConfigService],
-      // Using the 'useFactory' property to load the environment variables from the 'ConfigService'. This is the dependency injection design pattern.
       useFactory: (config: ConfigService) => {
-        // Returning the database configuration object.
         return {
-          // Using the 'type' property to specify the database type.
           type: 'sqlite',
-          // Using the 'database' property to specify the database name.
           database: config.get<string>('DB_NAME'),
-          // Using the 'entities' property to specify the entities.
           entities: [Users, Report],
-          // Using the 'synchronize' property to synchronize the database schema with the entities.
           synchronize: true,
         };
       },
     }),
-    // This is the old way of loading the environment variables from the 'ConfigService', it is commented out because it is not the dependency injection design pattern.
-    // TypeOrmModule.forRoot({
-    //   type: 'sqlite',
-    //   database: 'database.sqlite',
-    //   entities: [Users, Report],
-    //   synchronize: true,
-    // }),
     UsersModule,
     ReportsModule,
   ],
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -27,4 +27,31 @@ describe('Authentication System', () => {
         expect(email).toEqual(email);
       });
   });
+
+  // Add a test for signing up with a new user and then getting the currently logged in user.
+  it('signup as a new user then get the currently logged in user', async () => {
+    // Assign a email to a variable to use it in the test.
+    const email = 'testingemail213ew2@qa4free.com';
+    // Send a request and assign the response to a variable.
+    const response = await request(app.getHttpServer())
+      // Send a request to the signup endpoint.
+      .post('/auth/signup')
+      // Send the email and password in the body of the request.
+      .send({ email, password: 'password' })
+      // Expect a 201 status code.
+      .expect(201);
+    // Get the cookie from the response.
+    const cookie = response.get('Set-Cookie');
+    // Send a request to the whoami endpoint.
+    const { body } = await request(app.getHttpServer())
+      // Send the cookie in the header of the request.
+      .get('/auth/whoami')
+      // Set the cookie in the header of the request.
+      .set('Cookie', cookie)
+      // Expect a 200 status code.
+      .expect(200);
+
+    // Expect the body of the response to have the email that was sent in the signup request.
+    expect(body.email).toEqual(email);
+  });
 });
