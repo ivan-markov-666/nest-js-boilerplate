2800f53096997bc0db31f4f395dc69f9f750a5ef
Ivan Markov <101876348+ivan-markov-666@users.noreply.github.com>
2023-07-03 22:15:09 +0300

    Update reports.controller.ts

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -11,12 +11,12 @@ import {
 import { CreateReportDto } from './dto/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
-import { CurrentUser } from 'src/users/decorators/current-user.decorator';
-import { Users } from 'src/users/users.entity';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { Users } from '../users/users.entity';
 import { ReportDto } from './dto/report.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { ApproveReportDto } from './dto/approve-report.dto';
-import { AdminGuard } from 'src/guards/admin.guard';
+import { AdminGuard } from '../guards/admin.guard';
 import { GetEstimateDto } from './dto/get-estimate.dto';
 
 @Controller('reports')
